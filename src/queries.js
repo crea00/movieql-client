1ce889eb8e67e292b41da5757f10181586f811dc
crea00 <654321su@gmail.com>
2018-09-15 19:18:31 +0900

    #4 Getting data from the GraphQL API part One

---
+++ src/queries.js
@@ -0,0 +1,12 @@
+// Graphql-tag is the way that we can write graphQL on the frontend
+import gql from "graphql-tag";
+
+export const HOME_PAGE = gql`
+  {
+    movies(limit:50, rating:7) {
+      id
+      title
+      genres
+    }
+  }
+`
